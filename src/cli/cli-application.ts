6206cb574852a8daa52dd58457ddba973a98b40c
Elraden <morozovsa94@yandex.ru>
2024-01-14 15:45:41 +0300

    Добавлен менеджеру метод processCommand в CLIApplication.

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,9 +1,14 @@
+import { CommandParser } from './command-parser.js';
 import { Command } from './commands/command.interface.js';
 
 type CommandCollection = Record<string, Command>
 
 export class CLIApplication {
   private commands: CommandCollection = {};
+
+  constructor(
+    private readonly defaultCommand: string = '--help'
+  ) { }
 
   public registerCommands(commandList: Command[]): void {
     commandList.forEach((command) => {
@@ -13,5 +18,23 @@ export class CLIApplication {
       this.commands[command.getCommandName()] = command;
     });
   }
-}
 
+  public getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  public getDefaultCommand(): Command | never {
+    if (! this.commands[this.defaultCommand]) {
+      throw new Error(`Команда по умолчанию (${this.defaultCommand}) не зарегистрирована`);
+    }
+    return this.commands[this.defaultCommand];
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
+  }
+}
