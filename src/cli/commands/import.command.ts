41790e29382c1f58dfdf7ee6c14078be51e04e86
Elraden <morozovsa94@yandex.ru>
2024-01-28 18:44:24 +0300

    Добавлено выполнение команды import

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,4 +1,5 @@
 import { Command } from './command.interface.js';
+import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
 
 export class ImportCommand implements Command {
   public getCommandName(): string {
@@ -6,6 +7,20 @@ export class ImportCommand implements Command {
   }
 
   public execute(...params: string[]): void {
+    const [filename] = params;
+    const fileReader = new TSVFileReader(filename.trim());
 
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+
+      console.error(`Невозможно импортировать данные из файла ${filename}`);
+      console.error(`Детали: ${err.message}`);
+    }
   }
 }
