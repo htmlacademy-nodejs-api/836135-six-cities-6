61b4513b2509c5c0c3b06c14b17807c3164e2b55
Elraden <morozovsa94@yandex.ru>
2024-02-15 14:43:27 +0300

    Добавлен функционал получения данных из JSON

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,13 +1,35 @@
+import { MockServerData } from '../../types/index.js';
 import { Command } from './command.interface.js';
+import got from 'got';
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Невозможно загрузить данные из ${url}`);
+    }
+  }
+
   public getCommandName(): string {
     return '--generate';
   }
 
-  public execute(...params: string[]): void {
+  public async execute(...params: string[]): Promise<void> {
     const [count, filepath, url] = params;
     const offerCount = Number.parseInt(count, 10);
+
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Невозможно сгенерировать данные');
+
+      if(error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 
 }
