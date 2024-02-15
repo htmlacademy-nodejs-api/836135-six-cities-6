1b1085f2719796ab7b4210fd2d49319c7f95ed03
Elraden <morozovsa94@yandex.ru>
2024-02-15 15:10:59 +0300

    Регистрация команды генерации

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,11 +1,12 @@
-import { CLIApplication, HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
+import { CLIApplication, GenerateCommand, HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
   cliApplication.registerCommands([
     new HelpCommand(),
     new VersionCommand(),
-    new ImportCommand()
+    new ImportCommand(),
+    new GenerateCommand()
   ]);
 
   cliApplication.processCommand(process.argv);
