e10226bf0506c6f162028593b170cdcfcda26936
Elraden <morozovsa94@yandex.ru>
2024-02-15 14:51:35 +0300

    Добавлен функционал записи сгенерированных данных в файл

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,15 +1,30 @@
 import { MockServerData } from '../../types/index.js';
 import { Command } from './command.interface.js';
 import got from 'got';
+import { appendFile } from 'fs/promises';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
+import { errorColor, successColor } from '../../shared/libs/chalk/chalk.js';
+
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
+
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(
+        filepath,
+        `${tsvOfferGenerator.generate()}\n`,
+        { encoding: 'utf-8'}
+      );
+    }
+  }
 
   private async load(url: string) {
     try {
       this.initialData = await got.get(url).json();
     } catch {
-      throw new Error(`Невозможно загрузить данные из ${url}`);
+      throw new Error(errorColor(`Невозможно загрузить данные из ${url}`));
     }
   }
 
@@ -23,8 +38,10 @@ export class GenerateCommand implements Command {
 
     try {
       await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(successColor(`Файл ${filepath} был создан!`));
     } catch (error: unknown) {
-      console.error('Невозможно сгенерировать данные');
+      console.error(errorColor('Невозможно сгенерировать данные'));
 
       if(error instanceof Error) {
         console.error(error.message);
