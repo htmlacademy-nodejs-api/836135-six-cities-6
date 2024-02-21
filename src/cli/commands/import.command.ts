24d8cd1c6fd0f19c4538e6f6de8d4943991da4dc
Elraden <morozovsa94@yandex.ru>
2024-02-21 15:08:15 +0300

    Рефактор команды import

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,28 +1,35 @@
 import { Command } from './command.interface.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
-import { errorColor, warningColor } from '../../shared/libs/chalk/chalk.js';
-
+import { errorColor, warningColor, successColor } from '../../shared/libs/chalk/chalk.js';
+import { createOffer, getErrorMessage } from '../../shared/helpers/offer.js';
 
 export class ImportCommand implements Command {
   public getCommandName(): string {
     return '--import';
   }
 
-  public execute(...params: string[]): void {
+
+  private onImportedLine(line: string) {
+    const offer = createOffer(line);
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(successColor(`${count} строк импортировано`));
+  }
+
+  public async execute(...params: string[]): Promise<void> {
     const [filename] = params;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
+
     try {
-      fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (err) {
-
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-
-      console.error(`${errorColor('Невозможно импортировать данные из файла')} ${filename}`);
-      console.error(`${warningColor('Детали:')} ${err.message}`);
+      await fileReader.read();
+    } catch (error) {
+      console.error(errorColor(`Невозможно импортировать данные из файла ${filename}`));
+      console.error(errorColor(getErrorMessage(error)));
     }
   }
 }
