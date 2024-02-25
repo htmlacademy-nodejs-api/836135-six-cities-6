7b48d327024d79a8402f9fbba527b38ddf2939c5
Elraden <morozovsa94@yandex.ru>
2024-02-25 21:16:13 +0300

    добавлен dotenv

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,21 @@
 import { Config } from './config.interface.js';
+import { DotenvParseOutput, config } from 'dotenv';
+import { Logger } from '../logger/index.js';
 
 export class RestConfig implements Config {
+  private readonly config: NodeJS.ProcessEnv;
   constructor(
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+    private readonly logger: Logger
+  ) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Невозможно прочесть .env файл. Возможно файла не существует');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env файл найден и успешно обработан');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];
