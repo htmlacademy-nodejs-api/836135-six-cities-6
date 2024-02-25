53a8d4048822ec529bbb46ded599ef1a23b5977f
Elraden <morozovsa94@yandex.ru>
2024-02-25 21:42:58 +0300

    В схему добавлены переменные окружения

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -2,6 +2,8 @@ import convict from 'convict';
 
 export type RestSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -10,5 +12,17 @@ export const configRestSchema = convict<RestSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000
-  }
+  },
+  SALT: {
+    doc: 'Соль для хеширования пароля',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP адрес сервера базы данных (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  },
 });
