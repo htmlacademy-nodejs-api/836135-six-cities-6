831bc272339f14d92ed77aa01fc85f0d89c1990f
Elraden <morozovsa94@yandex.ru>
2024-03-02 15:08:05 +0300

    Добавлен функционал повторного подключения к БД

--- src/shared/libs/database-client/mongo.database-client.ts
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -1,8 +1,12 @@
 import * as Mongoose from 'mongoose';
 import { inject, injectable } from 'inversify';
+import { setTimeout } from 'node:timers/promises';
 import { DatabaseClient } from './database-client.interface.js';
 import { Component } from '../../../types/index.js';
 import { Logger } from '../logger/index.js';
+
+const RETRY_COUNT = 5;
+const RETRY_TIMEOUT = 1000;
 
 @injectable()
 export class MongoDatabaseClient implements DatabaseClient {
@@ -20,16 +24,27 @@ export class MongoDatabaseClient implements DatabaseClient {
   }
 
   public async connect(uri: string): Promise<void> {
-    if(this.isConnectedToDatabase()) {
+    if (this.isConnectedToDatabase()) {
       throw new Error('Клиент MongoDB уже подключен');
     }
 
     this.logger.info('Соедняемся с MongoDB...');
 
-    this.mongoose = await Mongoose.connect(uri);
-    this.isConnected = true;
+    let attempt = 0;
+    while (attempt < RETRY_COUNT) {
+      try {
+        this.mongoose = await Mongoose.connect(uri);
+        this.isConnected = true;
+        this.logger.info('Установлено соединения с базой данных');
+        return;
+      } catch (error) {
+        attempt++;
+        this.logger.error(`Ошибка подключения к БД. Попытка ${attempt}`, error as Error);
+        await setTimeout(RETRY_TIMEOUT);
+      }
+    }
 
-    this.logger.info('Установлено соединения с базой данных');
+    throw new Error(`Не удалось установить подключение к базе данных после ${RETRY_COUNT}`);
   }
 
   public async disconnect(): Promise<void> {
