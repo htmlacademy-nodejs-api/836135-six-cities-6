9960c9f442b122d366c3a7086eb67facc5168ccd
Elraden <morozovsa94@yandex.ru>
2024-03-03 14:48:27 +0300

    Добавлен сервис DefaultOfferService

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,12 +4,14 @@ import { RestApplication } from './rest/index.js';
 import { Component } from './types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
 import { createUserContainer } from './shared/modules/user/user.container.js';
+import { createRentalOfferContainer } from './shared/modules/offer/offer.container.js';
 
 
 async function bootstrap() {
   const appContainer = Container.merge(
     createRestApplicationContainer(),
     createUserContainer(),
+    createRentalOfferContainer(),
   );
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
--- src/types/component.enum.ts
+++ src/types/component.enum.ts
@@ -5,4 +5,6 @@ export const Component = {
   DatabaseClient: Symbol.for('DatabaseClient'),
   UserService: Symbol.for('UserService'),
   UserModel: Symbol.for('UserModel'),
+  RentalOfferService: Symbol.for('RentalOfferService'),
+  RentalOfferModel: Symbol.for('RentalOfferModel'),
 } as const;
