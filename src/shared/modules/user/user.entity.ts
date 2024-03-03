421080211f01b7965d5f075e6a8ce82109d32103
Elraden <morozovsa94@yandex.ru>
2024-03-03 10:17:13 +0300

    Добавлены поля createdAt, updated Ad, _id, id

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,23 @@
+import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
 import { User } from '../../../types/index.js';
 
-export class UserEntity implements User {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class UserEntity extends defaultClasses.TimeStamps implements User {
+  @prop({ required: true })
   public username: string;
+
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: true })
   public password: string;
+
+  @prop({ required: false, default: '' })
   public avatar: string;
+
+  @prop({ required: true })
   public isPro: boolean;
 }
