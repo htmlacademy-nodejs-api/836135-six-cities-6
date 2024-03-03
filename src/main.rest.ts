ae087d71b404f62669cdf27d8ea3cc015b372be2
Elraden <morozovsa94@yandex.ru>
2024-03-03 11:12:38 +0300

    Разбиение общего контейнера на userContainer и restContainer. Рефактор main.rest

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,20 +1,17 @@
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { Container } from 'inversify';
 import 'reflect-metadata';
 import { RestApplication } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './types/index.js';
-import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
+import { createUserContainer } from './shared/modules/user/user.container.js';
 
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
-
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+  );
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
